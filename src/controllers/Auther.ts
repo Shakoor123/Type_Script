3eee4be4de57fa49f91358a4bf98db916b4e2fbc
shakoor123 <shakoorchachu2001@gmail.com>
2022-06-29 01:29:07 +0530

    auther functions added

--- src/controllers/Auther.ts
+++ src/controllers/Auther.ts
@@ -1,7 +1,8 @@
 import Auther from '../models/Auther';
 import mongoose from 'mongoose';
+import { NextFunction } from 'express';
 
-const crateAuther = (req: Request, res: Response) => {
+const crateAuther = (req: Request, res: Response, next: NextFunction) => {
     const auther = new Auther({
         _id: new mongoose.Types.ObjectId(),
         name: req.body
@@ -11,6 +12,53 @@ const crateAuther = (req: Request, res: Response) => {
         .then((auther) => res.status(201).json({ auther }))
         .catch((err) => res.status(500).json({ err }));
 };
-const readAllAuther = (req: Request, res: Response) => {};
-const UpdateAuther = (req: Request, res: Response) => {};
-const DeleteAuther = (req: Request, res: Response) => {};
+const readAuther = (req: Request, res: Response,next:NextFunction) => {
+    const userId = req.params.id;
+    return Auther.findById(userId)
+        .then((auther) => {
+            auther ? (res.status(200).json(auther);
+        }))
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+};
+const readAllAuther = (req: Request, res: Response, next: NextFunction) => {
+    
+    return Auther.find()
+        .then((authers) => {
+            authers ? (res.status(200).json(authers);
+        }))
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+};
+const UpdateAuther = (req: Request, res: Response, next: NextFunction) => {
+     const userId = req.params.id;
+    return Auther.findById(userId)
+        .then((auther) => {
+           if(auther){
+            auther.set(req.body);
+            return auther.save()
+            .then((auther) => {
+            auther ? (res.status(200).json(auther);
+        }))
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+
+           }else{
+            res.status(401).json({{message:"not found"}});
+           }
+        }))
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+};
+const DeleteAuther = (req: Request, res: Response, next: NextFunction) => {
+    const autherId=req.params.id;
+    return Auther.findByIdAndDelete(autherId)
+    .then((auther)=>(auther?res.status(201).json(auther):res.status(404)))
+    .catch((err)=>res.status(500).json(err));
+};
+
+export default {crateAuther,readAuther,readAllAuther,UpdateAuther,DeleteAuther};
